e770039b5f3ac574e158681d9b357e96c4c20930
realzvqle <realzvqle@gmail.com>
2024-10-31 16:16:14 -0500

    console stuff

--- src/WinTSP.c
+++ src/WinTSP.c
@@ -7,7 +7,28 @@
 
 
 int wmain()
-{   
+{
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE)
+    {
+        show_failure_resp(GetLastError());
+        return -1;
+    }
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode))
+    {
+        show_failure_resp(GetLastError());
+        return -2;
+    }
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    if (!SetConsoleMode(hOut, dwMode))
+    {
+        show_failure_resp(GetLastError());
+        return -3;
+    }
+
     start_shell();
     return 0;
 }
